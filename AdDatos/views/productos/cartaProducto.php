683d1e1e581df37fa7ec9e2821e7be68da6da9d9
victorbg202 <victorbg202@gmail.com>
2020-12-15 17:06:07 +0100

    15/12/2020 prova

--- AdDatos/views/productos/cartaProducto.php
+++ AdDatos/views/productos/cartaProducto.php
@@ -4,18 +4,16 @@
 <div class="container, containerProd">
   <div class="card, cardProd">
     <div class="card-head, card-headCard">
-      <img src="https://s5.postimg.cc/j9r8yf9gn/sws1.png" alt="Shoe" class="product-imgCard">
+      <img src="assets/img/monitor.svg" alt="Shoe" class="product-imgCard">
       <div class="product-detail">
-        <h2>Hartbeespoort</h2> Support and Nike Zoom Air come together for a more supportive feel with high-speed responsiveness
+        <h2>Hartbeespoort</h2>
       </div>
-      <span class="back-text">
-              FAS
-            </span>
+      <span class="back-text"> LG</span>
     </div>
     <div class="card-body, card-bodyCard">
-      <h1>Hola</h1>
+      <br>
+      <h1>LG 4K 5415L</h1>
       <p>Lorem ipsum dolor sit amet, consectetur elit. Similique voluptatem id error facilis molestiae, at rep unde voluptatum.</p>
-      <button class="btn btn-warning">Comprar</button>
     </div>
   </div>
 </div>
